95bc6e7422ffb387a88d8b273e13812c7005e11b
Jielin Li <jielinli@Jielins-MBP.attlocal.net>
2025-02-16 20:20:29 -0800

    add signout support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,41 @@ export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 */
+"use client"
 import { Amplify } from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css"
-import { ThemeProvider } from '@aws-amplify/ui-react';
-import {studioTheme}  from '../src/ui-components';
-
+import { Divider, ThemeProvider } from '@aws-amplify/ui-react';
+import {studioTheme, CarRentalNavBar}  from '../src/ui-components';
+import {signOut} from "aws-amplify/auth"
 import awsExports from "../src/aws-exports.js";
 import React, { FC } from "react";
 
 Amplify.configure(awsExports, {ssr: true});
 
 function MyApp({ Component, pageProps }: any ) {
+  async function handleSignOut() {
+    await signOut()
+  }
+
+  const navbarOverrides = {
+    "image38601372" : {
+      src: "https://img.icons8.com/?size=100&id=15126&format=png&color=000000"
+    },
+    "image3860799" : {
+      src: "avatar1.png",
+    }, 
+    "Button" : {
+      children: "Sign Out",
+      buttonOnClick: signOut,
+    }
+  }
+ 
+
   // Use the Layout component to wrap every page
   return (
     <ThemeProvider theme = {studioTheme}>
+       <CarRentalNavBar overrides = {navbarOverrides}/>      
+       <Divider/>
       <Component {...pageProps} />
     </ThemeProvider>
   );
